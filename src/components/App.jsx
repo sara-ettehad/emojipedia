df7871d1360f62a41db331c0007ebbac3d87a667
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2023-06-26 20:04:24 -0400

    some changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
 
-function App(){
-    return(
-        <div></div>
-    )
+function createEntry(emojiTerm){
+    return (
+        <Entry 
+            key={emojiTerm.id}
+            emoji={emojiTerm.emoji}
+            name={emojiTerm.name}
+            description={emojiTerm.description}
+        />
+    );
 }
+
+function App() {
+    return (
+        <div>
+            <h1>
+                <span>Emojipedia</span>
+            </h1>
+            <dl className='dictionary'>{emojipedia.map(createEntry)}</dl>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,28 @@
+const emojipedia =[
+    {
+        id: 1,
+        emoji: "💪🏻",
+        name: "Tense Biceps",
+        description: "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."  
+    },
+    {
+        id: 2,
+        emoji: "🙏",
+        name: "Person With Folded Hands",
+        description: "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+        id: 3,
+        emoji: "🤣",
+        name: "Rolling On The Floor, Laughing",
+        description: "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+        id: 4,
+        emoji: "🤓",
+        name: "Nerd Face",
+        description:"Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+    }
+];
+
+export default emojipedia;
